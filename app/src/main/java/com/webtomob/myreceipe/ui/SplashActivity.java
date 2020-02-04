6727aa03337a8951842b0a3352f71787c154fdc6
ankit <akki3901@yahoo.co.in>
2020-02-04 16:13:56 +0800

    - Initial Commit Adding Splash and Home Screen

---
+++ app/src/main/java/com/webtomob/myreceipe/ui/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.webtomob.myreceipe.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.webtomob.myreceipe.R;
+import com.webtomob.myreceipe.ui.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        creatingSplashScreen();
+    }
+
+    /********* Creating Splash Screen ***********/
+
+    private void creatingSplashScreen() {
+
+        new CountDownTimer(2500, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                Intent in = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(in);
+                finish();
+            }
+        }.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
---
+++ app/src/main/java/com/webtomob/myreceipe/ui/home/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.webtomob.myreceipe.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.webtomob.myreceipe.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+
+
+    }
+
+}
